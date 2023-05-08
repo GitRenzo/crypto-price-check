4e9b85e5ef38b41e76db040f861f4620cbae27b6
Renzo <renzo@ETTEs-Mac-mini-Cami.local>
2023-05-08 16:06:00 -0400

    First commit

---
+++ src/Hooks/useSelectMonedas.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Label = styled.label`
+    color: #FFF;
+`
+
+const useSelectMonedas = (label) => {
+    const SelectMonedas = () => (
+        <>
+            <Label >{label}</Label>
+        </>
+    )
+    return [SelectMonedas]
+}
+
+export default useSelectMonedas
